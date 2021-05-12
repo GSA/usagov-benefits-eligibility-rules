e08b1c6c4872812a3292dbfbb435282c1ba19000
Taylor Zajicek <tzajicek@flexion.us>
2021-05-12 16:16:33 -0500

    BEARS-56 - Add VA example

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import { Engine } from 'json-rules-engine';
 import rules from './bears.json';
 
+const onSuccess = async (event, almanac) => {
+  almanac.addRuntimeFact('benefit.va.bronzeMedalion', 1);
+};
+
+const onFailure = async (event, almanac) => {
+  almanac.addRuntimeFact('benefit.va.bronzeMedalion', 0);
+};
+
 export function config() {
   return Promise.resolve();
 };
 
 export function isElegible({ facts }) {
-  const rulesEngine = new Engine(rules.decisions);
+
+  rules.decisions.filter(d => d.event.type === 'benefit.va.bronzeMedalion').forEach((d) => {
+    d.onSuccess = onSuccess;
+    d.onFailure = onFailure;
+  });
+
+  const rulesEngine = new Engine(rules.decisions, {
+    allowUndefinedFacts: true,
+  });
   return rulesEngine.run(facts);
 };
