be1da7cee72cae1ddf7f9d8e3055de1f45e8db00
Taylor Zajicek <tzajicek@flexion.us>
2021-05-18 08:34:37 -0500

    Spelling

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import { Engine } from 'json-rules-engine';
 import rules from './bears.json';
 
-const onSuccess = async (event, almanac) => {
-  almanac.addRuntimeFact('benefit.va.bronzeMedalion', 1);
-};
-
-const onFailure = async (event, almanac) => {
-  almanac.addRuntimeFact('benefit.va.bronzeMedalion', 0);
-};
-
 export function config() {
   return Promise.resolve();
 };
 
-export function isElegible({ facts }) {
-
-  rules.decisions.filter(d => d.event.type === 'benefit.va.bronzeMedalion').forEach((d) => {
-    d.onSuccess = onSuccess;
-    d.onFailure = onFailure;
-  });
-
+export function isEligible({ facts }) {
   const rulesEngine = new Engine(rules.decisions, {
     allowUndefinedFacts: true,
   });
